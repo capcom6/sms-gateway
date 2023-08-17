f96147ca50019b3032cd4d0b951a09f9e5903d9f
Aleksandr Soloshenko <capcom2me@gmail.com>
2023-08-17 23:26:48 +0700

    Added: Terraform and Docker Swarm support

---
+++ deployments/docker-swarm-terraform/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+  host     = var.swarm-manager-host
+  ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
+  registry_auth {
+    address  = "cr.selcloud.ru/soft-c"
+    username = "token"
+    password = var.registry-password
+  }
+}
---
+++ deployments/docker-swarm-terraform/variables.tf
@@ -0,0 +1,46 @@
+variable "swarm-manager-host" {
+  type        = string
+  sensitive   = true
+  description = "Address of swarm manager"
+}
+
+variable "registry-password" {
+  type        = string
+  description = "Password for Docker Images Registry"
+  sensitive   = true
+}
+
+variable "app-name" {
+  type        = string
+  description = "Name of app"
+}
+
+variable "app-version" {
+  type        = string
+  description = "Version of Docker image of app"
+  default     = "1.0"
+}
+
+variable "app-host" {
+  type        = string
+  description = "Hostname of app"
+}
+
+variable "app-config-b64" {
+  type        = string
+  description = "Application config file, base64 encoded"
+  sensitive   = true
+}
+
+variable "app-env-json-b64" {
+  type        = string
+  description = "Evironment variables, base64 encoded JSON"
+  sensitive   = true
+}
+
+variable "memory-limit" {
+  type        = number
+  description = "Memory limit in bytes"
+  default     = 32 * 1024 * 1024
+}
+
