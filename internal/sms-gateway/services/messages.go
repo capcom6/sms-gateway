cd95d8a5a2504188aa6584d38b714540dfe4ee8a
Aleksandr Soloshenko <capcom2me@gmail.com>
2024-02-23 00:04:30 +0700

    [logging][messages] log message state update errors

--- internal/sms-gateway/services/messages.go
+++ internal/sms-gateway/services/messages.go
@@ -53,7 +53,7 @@ func NewMessagesService(params MessagesServiceParams) *MessagesService {
 	return &MessagesService{
 		Messages: params.Messages,
 		PushSvc:  params.PushSvc,
-		Logger:   params.Logger,
+		Logger:   params.Logger.Named("MessagesService"),
 		idgen:    idgen,
 	}
 }
@@ -206,7 +206,9 @@ func (s *MessagesService) filterTimeouted(messages []models.Message) []models.Me
 				v.Recipients[i].State = models.MessageStateFailed
 				v.Recipients[i].Error = types.AsPointer(ErrorTTLExpired)
 			}
-			s.Messages.UpdateState(&v)
+			if err := s.Messages.UpdateState(&v); err != nil {
+				s.Logger.Error("Can't update message state", zap.Error(err))
+			}
 		}
 	}
 	return result
