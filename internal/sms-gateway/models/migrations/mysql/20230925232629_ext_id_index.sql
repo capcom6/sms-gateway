a011d5e526538afd7a03859dee1bde4c888396e2
Aleksandr Soloshenko <capcom2me@gmail.com>
2024-11-05 06:11:00 +0700

    [migrations] fix compatibility with MySQL

--- internal/sms-gateway/models/migrations/mysql/20230925232629_ext_id_index.sql
+++ internal/sms-gateway/models/migrations/mysql/20230925232629_ext_id_index.sql
@@ -1,15 +1,15 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE UNIQUE INDEX IF NOT EXISTS `unq_messages_id_device` ON `messages`(`ext_id`, `device_id`);
+CREATE UNIQUE INDEX `unq_messages_id_device` ON `messages`(`ext_id`, `device_id`);
 -- +goose StatementEnd
 -- +goose StatementBegin
-DROP INDEX IF EXISTS `unq_messages_device_id` ON `messages`;
+DROP INDEX `unq_messages_device_id` ON `messages`;
 -- +goose StatementEnd
 --
 -- +goose Down
 -- +goose StatementBegin
-CREATE UNIQUE INDEX IF NOT EXISTS `unq_messages_device_id` ON `messages`(`device_id`, `ext_id`);
+CREATE UNIQUE INDEX `unq_messages_device_id` ON `messages`(`device_id`, `ext_id`);
 -- +goose StatementEnd
 -- +goose StatementBegin
-DROP INDEX IF EXISTS `unq_messages_id_device` ON `messages`;
+DROP INDEX `unq_messages_id_device` ON `messages`;
 -- +goose StatementEnd
